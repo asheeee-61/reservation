2243ff2d470ebe61eb7451b07e9f79c6d1d73d5a
Ashe <achrafwhosassad@gmail.com>
2026-03-26 01:31:47 +0100

    u

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Catch-all for React SPA
+Route::get('/{any}', function () {
+    return response()->file(public_path('index.html'));
+})->where('any', '.*');
+
