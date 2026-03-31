49aa5d6f553101e4ec80a711eeb6a83c65eb247c
lahbouch <lahbouch.dev@gmail.com>
2026-04-01 01:50:05 +0200

    chore: remove unused configuration file

--- backend-repo/config/notice.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'url' => env('NOTICE_SYSTEM_URL'),
-    'secret' => env('NOTICE_SYSTEM_SECRET'),
-    'admin_phone' => env('RESTAURANT_PHONE'),
-];
--- backend-repo/routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-// Catch-all for React SPA
-Route::get('/{any}', function () {
-    return response()->file(public_path('index.html'));
-})->where('any', '.*');
-
